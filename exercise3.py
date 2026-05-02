5884f261296ca662ffc0e58a0f92de9f09e47c48
UmairPatel <umairryme21@gmail.com>
2026-05-02 18:02:55 +0200

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,17 @@
+# Exercise 2 
+
+initial_balance = 10000
+transactions = [200, -500, 300, -1000, 400, -200, 150, -50] 
+
+def finBal(initial_balance, transactions):
+    return initial_balance + sum(transactions)
+
+def count_withdrawels(transactions):
+    withdrawels = 0
+    for amount in transactions:
+        if amount < 0:
+            withdrawels += 1
+    return withdrawels        
+
+print(f"The total balance is: ${finBal (initial_balance, transactions):,.1f}")
+print(f"The amount of withdrawels is: {count_withdrawels (transactions)}")
